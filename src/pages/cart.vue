a2e1bc78c8e798049406e4c78a600bded1a644bb
papa <916314935@qq.com>
2019-09-05 10:39:18 +0800

    szr完成个人页面静态

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,13 +1,22 @@
 <template>
-  <div>空购物车</div>
+  <div>
+    <van-nav-bar title="购物车" border="true" @click-left="onClickLeft" id="qwe">
+      <van-icon name="arrow-left" slot="left" />
+    </van-nav-bar>
+  
+    
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+#qwe {
+  height: 50px;
+  line-height: 50px;
+  font-size: 18px;
+}
 
 </style>
