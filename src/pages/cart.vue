a45ef4fb498d51e85251d1c3bb8e3420cbc4fe4a
papa <916314935@qq.com>
2019-09-06 17:11:57 +0800

    更新我的页面,完成详情页静态

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -1,22 +1,13 @@
 <template>
-  <div>
-    <van-nav-bar title="购物车" border="true" @click-left="onClickLeft" id="qwe">
-      <van-icon name="arrow-left" slot="left" />
-    </van-nav-bar>
   
-    
-  </div>
 </template>
 
 <script>
-export default {};
+export default {
+ 
+};
 </script>
 
-<style>
-#qwe {
-  height: 50px;
-  line-height: 50px;
-  font-size: 18px;
-}
+<style scoped>
 
 </style>
